ec204ac9dd3641d64572e66a0c346fdc74e05325
pallavigaikwad24 <gaikwadpallavi263@gmail.com>
2025-01-20 17:38:38 +0530

    Updated getModelInfo Method

--- src/services/getModelInfo.js
+++ src/services/getModelInfo.js
@@ -21,33 +21,3 @@ const getModelInfo = async ({ modelName, methodType, args }) => {
 };
 
 module.exports = getModelInfo;
-
-
-src/Authentication/passportAuth.js src/controller/FlightController/cancellingFlightBookingController.js src/controller/FlightController/deleteFlightBookingController.js
-src/controller/FlightController/flightBookingController.js
-src/controller/FlightController/flightSearchController.js
-src/controller/FlightController/updateFlightController.js
-src/controller/HotelController/cancellingHotelBookingController.js
-src/controller/HotelController/deleteHotelBookingController.js
-src/controller/HotelController/deleteHotelController.js
-src/controller/HotelController/hotelBookingController.js
-src/controller/HotelController/hotelRecordsController.js
-src/controller/HotelController/hotelSearchController.js
-src/controller/HotelController/updateHotelController.js
-src/controller/HotelController/verifyHotelController.js
-src/controller/UserController/emailForgetPasswordController.js
-src/controller/UserController/emailVerificationController.js
-src/controller/UserController/firebaseController.js
-src/controller/UserController/forgetPasswordController.js
-src/controller/UserController/loginController.js
-src/middleware/emailAuthMiddleware.js
-src/middleware/emailVerificationMiddleware.js
-src/middleware/flightBookingMiddleware.js
-src/middleware/flightSearchMiddleware.js
-src/middleware/hotelBookingMiddleware.js
-src/middleware/hotelSearchMiddleware.js
-src/middleware/otpLoginMiddleware.js
-src/middleware/registrationMiddleware.js
-src/routes/searchRoute.js
-src/services/getModelInfo.js
-src/services/getUserInfo.js
